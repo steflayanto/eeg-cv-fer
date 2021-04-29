e075d7c5540b02fc0d7074e2aa10db5dc06304a0
Stefan Layanto <stef.layanto@gmail.com>
2021-04-29 11:14:46 -0700

    added cli arg parser

---
+++ dev/modelRunner.py
@@ -0,0 +1,40 @@
+import sys, getopt
+
+usage = "Usage: modelRunner.py [-n, --modelName] cv-default-mtcnn [-f, --labelFrequency] 2 [-d, --dataPath] data/video_or_eegfilename"
+
+def main(argv):
+    model_name, sample_rate, data_path = None, None, None
+    try:
+        opts, args = getopt.getopt(argv,"hn:f:d:",["modelName=","labelFrequency=","dataPath="])
+    except getopt.GetoptError:
+        print(usage)
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            print(usage)
+            sys.exit()
+        elif opt in ("-n", "--modelName"):
+            model_name = arg
+        elif opt in ("-f", "--labelFrequency"):
+            try:
+                sample_rate = int(arg)
+                if not sample_rate:
+                    print("Error parsing labelFrequency: ", arg)
+                    sys.exit()
+            except Exception as e:
+                print("Error parsing labelFrequency: ", e)
+                sys.exit()
+        elif opt in ("-d", "--dataPath"):
+            data_path = arg
+    if None in (model_name, sample_rate, data_path):
+        print("Error: Missing an argument.")
+        print(usage)
+        sys.exit()
+    return model_name, sample_rate, data_path
+
+if __name__ == "__main__":
+    ret = main(sys.argv[1:])
+    if not ret:
+        print("Error parsing args")
+    model_name, sample_rate, data_path = ret
+    print('Running model: {}, at frequency {} Hz, on data: {}'.format(model_name, sample_rate, data_path))
